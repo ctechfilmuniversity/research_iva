2fbd8a850d30705e9a2abd482680082832b7ce31
Markus Traber <markus.traber@gmail.com>
2021-01-03 20:29:03 +0100

    Now Stimberg app is basically working on web, but it is much more nervous than the original app. The flow detection might have sth to do with that.

--- web_iva/stimberg/sketch.js
+++ web_iva/stimberg/sketch.js
@@ -7,20 +7,13 @@ const tones = ['A2', 'C4', 'G4', 'E4', 'A1', 'E5', 'B4', 'C5'];
 let gainNodes = [];
 let oscNodes = [];
 
-//const osc = new Tone.Oscillator('E3', 'sine').toDestination().start();
-
 function setup() {
   createCanvas(IVA_WIDTH, IVA_HEIGHT);
-
-  // cam = createCapture(VIDEO);
-  // cam.size(IVA_WIDTH, IVA_HEIGHT);
-  // cam.hide();
 
   stepSize = 10;
   ySteps = IVA_WIDTH / stepSize;
   xSteps = IVA_HEIGHT / stepSize;
 
-  // new approach async?
   flow = new oflow.WebCamFlow(null, stepSize);
   // Every time when optical flow is calculated
   // call the passed in callback:
@@ -57,65 +50,53 @@ function draw() {
   // col = cam.get(300, 0, 1, cam.height);
   // image(col, slitIndex(), 0, 1, height);
 
-  drawFlowVectors();
+  flowVectorOutput();
 }
 
-function drawFlowVectors() {
+function flowVectorOutput() {
   strokeWeight(1);
   if (currentFlow && currentFlow.zones) {
-    currentFlow.zones.forEach(function(zone) {
+    // tone generation
+    const newAmp = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+    const iTotal = xSteps * ySteps;
+
+    currentFlow.zones.forEach(zone => {
+      // drawing the flow vectors
       //stroke(map(zone.u, -stepSize, +stepSize, 0, 255), map(zone.v, -stepSize, +stepSize, 0, 255), 128);
       stroke(255);
       line(zone.x, zone.y, zone.x + zone.u, zone.y + zone.v);
+
+      
+      // generating sound
+      if (zone.u == 0 || zone.v == 0) return;
+
+      // calc angle
+      translate(zone.x, zone.y);
+      let angle = atan2(zone.v, zone.u); // radians y, x
+      let offset = createVector(zone.u, zone.v).mag().toFixed(2);
+      translate(- zone.x, - zone.y);
+
+      angle = degrees(angle).toFixed(0) // to degrees
+      if (angle < 0) angle = 180 - angle; // having degrees until 360
+
+      if (angle < 90) {
+        newAmp[0] += 1.0 / iTotal;
+        newAmp[4] += offset / iTotal * 0.25;
+      } else if (angle < 180) {
+        newAmp[1] += 1.0 / iTotal;
+        newAmp[5] += offset / iTotal * 0.25;
+      } else if (angle < 270) {
+        newAmp[2] += 1.0 / iTotal;
+        newAmp[6] += offset / iTotal * 0.25;
+      } else  {
+        newAmp[3] += 1.0 / iTotal;
+        newAmp[7] += offset / iTotal * 0.25;
+      }
     });
-  }
-}
 
-function calculateGains() {
-  let i = 0;
-  let iTotal = xSteps * ySteps;
-  
-  const newAmp = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
-
-  currentFlow.zones.forEach(zone => {
-    // calc angle
-    // const v1 = createVector(-1, 1);
-    // const v2 = createVector(zone.u, zone.v);
-    // let angle = v1.angleBetween(v2);
-
-    translate(zone.x, zone.y);
-    let angle = atan2(zone.v, zone.u);
-
-    if (zone.u == 0 || zone.v == 0) {
-      console.log("Skpppe");
-      return;
+    for (let i = 0; i < 8; i++) {
+      //amps[i].set(newAmp[i]);
+      gainNodes[i].gain.rampTo(newAmp[i], .2); // change gain within 1 second
     }
-
-    console.log("?==0====");
-    //console.log(angle);
-    console.log(degrees(angle));
-
-    // if (angle < 90) {
-    //   newAmp[0] += 1.0 / iTotal;
-    //   newAmp[4] += offset.length() / iTotal * 0.25;
-    // } else if (angle < 180) {
-    //   newAmp[1] += 1.0 / iTotal;
-    //   newAmp[5] += offset.length() / iTotal * 0.25;
-    // } else if (angle < 270) {
-    //   newAmp[2] += 1.0 / iTotal;
-    //   newAmp[6] += offset.length() / iTotal * 0.25;
-    // } else  {
-    //   newAmp[3] += 1.0 / iTotal;
-    //   newAmp[7] += offset.length() / iTotal * 0.25;
-    // }
-  });
-      
-      
-        
-      
-
-
-  // for (let i = 0; i < gainNodes.length; i++) {
-  //   gainNodes[i].rampTo(newAmp[i], 1);
-  // }
+  }
 }
